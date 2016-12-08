8bbd5bdead192b1bf2ffe1e2e7d24a54c723acbd
Jack Ross, III <jack.ross@bookit.com>
2016-12-08 14:48:43 -0500

    Dynamically responding based on the contents of the message sent

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
     "fmt"
+    "io"
+    "io/ioutil"
     "log"
     "net/http"
-    "io/ioutil"
+    "strings"
 
+    // Third party code for routing
     "gorilla/mux"
 )
 
@@ -30,10 +33,21 @@ func ExampleId(response http.ResponseWriter, request *http.Request) {
 }
 
 func Worldspan(response http.ResponseWriter, request *http.Request) {
+    body, error := ioutil.ReadAll(io.LimitReader(request.Body, 1048576));
+    if (error != nil) {
+        log.Fatal(error);
+    }
+    if (strings.Contains(string(body), "<PSC5>")) {
+        HandlePowerShopperRequest(response);
+    } else {
+        fmt.Fprintln(response, "Type of request not found");
+    }
+}
+
+func HandlePowerShopperRequest(response http.ResponseWriter) {
     // @TODO Ideally, we wouldn't be using the full path here
-    data, error := ioutil.ReadFile(
-        "/go/src/worldspan-simulator/data/testPowerShopperResponse"
-    );
+    // @TODO Also, couldn't figure out how to break up the line below
+    data, error := ioutil.ReadFile("/go/src/worldspan-simulator/data/testPowerShopperResponse");
     if (error != nil) {
         log.Fatal(error);
     }
