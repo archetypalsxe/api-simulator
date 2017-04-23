47b788d53550521fa9b5386fd0662613999418b0
Jack Ross, III <mrjackross@gmail.com>
2017-04-23 16:07:56 -0400

    Added additional automated tests, improved functionality

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "io"
     "io/ioutil"
     "log"
     "net/http"
@@ -10,24 +11,32 @@ import (
 )
 
 func TestRootDirectory(test *testing.T) {
-    sendRequest(test,
+    sendRequestForm(test,
         "http://localhost:6060",
         url.Values{},
         "Root directory of the API simulator")
 }
 
 func TestExampleId(test *testing.T) {
-    sendRequest(test,
+    sendRequestForm(test,
         "http://localhost:6060/example/5",
         url.Values{},
         "Provided ID:  5")
 }
+
+func TestWorldspanPowerShopper(test *testing.T) {
+    sendRequestPost(test,
+        "http://localhost:6060/worldspan",
+        strings.NewReader("<PSC5>"),
+        "PSW5")
+}
         //url.Values{"key": {"Value"}, "id": {"123"}})
 
-func sendRequest(test *testing.T,
+func sendRequestForm(test *testing.T,
     url string,
     urlValues url.Values,
     expectedText string) {
+
     response, error := http.PostForm(url, urlValues)
     if error != nil {
         log.Fatal(error)
@@ -39,9 +48,38 @@ func sendRequest(test *testing.T,
         log.Fatal(error)
     }
 
-    if(!strings.Contains(string(body),
-        expectedText)) {
-            test.Error("Did not find expected text: '" + expectedText +
-            "' Received: '"+ strings.TrimSpace(string(body)) + "'")
+    validateResponse(test, strings.TrimSpace(string(body)), expectedText)
+
+}
+
+func sendRequestPost(test *testing.T,
+    url string,
+    data io.Reader,
+    expectedText string) {
+
+    request, error := http.NewRequest("POST",
+        url,
+        data)
+    if error != nil {
+        log.Fatal(error)
+    }
+
+    httpClient := http.Client{}
+    response, error := httpClient.Do(request)
+    if error != nil {
+        log.Fatal(error)
+    }
+    body, error := ioutil.ReadAll(response.Body)
+    if error != nil {
+        log.Fatal(error)
+    }
+    validateResponse(test, strings.TrimSpace(string(body)), expectedText)
+}
+
+func validateResponse(test *testing.T, actual string, expected string) {
+    if(!strings.Contains(actual,
+        expected)) {
+            test.Error("Did not find expected text: '" + expected +
+            "' Received: '"+ actual + "'")
     }
 }
