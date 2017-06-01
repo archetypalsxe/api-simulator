a580e7ddd09e8c2f750fa1955b02d945be08700a
Jack Ross, III <mrjackross@gmail.com>
2017-06-01 15:47:12 -0400

    Working on form for settings. Just about done with ability to modify existing settings, with the exception of not actually being able to save

--- js/settings.js
+++ js/settings.js
@@ -1,1 +1,50 @@
+$(document).ready(function() {
+    $('.cursor').click(function(event) {
+        clickEvent(event);
+    });
+});
 
+function clickEvent(event) {
+    var target = $(event.target);
+    var targetId = target.attr('id');
+
+    if(target.data('type') == "pre") {
+        var type = "textarea";
+    } else {
+        var type = "input";
+    }
+
+    target.replaceWith($('<'+type+'/>', {
+        'id': targetId,
+        'class': target.attr('class'),
+        'type': 'text',
+        'value': target.text().trim(),
+        'val': target.text().trim(),
+        'data-type': target.data('type'),
+        'data-original': target.text().trim()
+    }));
+
+    var target = $('#'+targetId);
+    target.focus();
+    target.blur(function(event) {
+        blurEvent(event);
+    });
+}
+
+function blurEvent(event) {
+    var target = $(event.target);
+    var targetId = target.attr('id');
+
+    var confirmResult = window.confirm("Save changes?");
+
+    target.replaceWith($('<'+target.data('type')+'/>', {
+        'id': targetId,
+        'class': target.attr('class'),
+        'text': ((confirmResult) ? target.val() : target.data('original')),
+        'data-type': target.data('type')
+    }));
+    var target = $('#'+targetId);
+    target.click(function(event) {
+        clickEvent(event);
+    });
+}
